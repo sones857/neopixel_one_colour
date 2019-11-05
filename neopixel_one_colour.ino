9577ec4438d7ed19c43cd20c92cb3bda8bca7670
Tom Sparrow <sones857@users.noreply.github.com>
2019-11-05 18:17:57 +0000

    Add files via upload

---
+++ neopixel_one_colour.ino
@@ -0,0 +1,41 @@
+#include <Adafruit_NeoPixel.h>
+#define PIN 2
+#define NUM_LIGHTS  30
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LIGHTS, PIN, NEO_GRB + NEO_KHZ800);
+
+void setup() {
+  strip.begin();
+  strip.show();
+}
+
+void loop() {
+  uint32_t low = strip.Color(0, 0, 0);
+  uint32_t high = strip.Color(255, 0, 0);
+
+  /*
+    ##########################################################
+    ##################  COLOUR VALUES  #######################
+    ##########################################################
+
+    ###############     RED  255, 0, 0       #################
+    ###############    GREEN 0, 255, 0       #################
+    ###############     BLUE 0, 0, 255       #################
+    ###############     CYAN 0, 255, 255     #################    
+    ###############   PURPLE 128, 0, 128     #################
+    ###############  MAGENTA 255, 0, 255     ################# 
+    ###############   YELLOW 255, 255, 0     #################
+    ###############   ORANGE 255, 165, 0     #################
+    ###############     WARM 255, 233, 233   #################
+    ###############    WHITE 255, 255, 255   #################
+    
+  */
+
+  // Turn them off
+
+  for ( int i = 0; i < NUM_LIGHTS; i++) {
+    strip.setPixelColor(i, high);
+    strip.show();
+  }
+  delay(10);
+}
